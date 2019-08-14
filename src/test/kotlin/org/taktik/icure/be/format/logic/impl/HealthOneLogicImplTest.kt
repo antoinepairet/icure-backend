0b337f47a98c9f46f96d942f1db16a84fd93d045
Julia Dumur <dumur.julia@gmail.com>
2019-08-14 18:44:42 +0200

    fake commit

--- src/test/kotlin/org/taktik/icure/be/format/logic/impl/HealthOneLogicImplTest.kt
+++ src/test/kotlin/org/taktik/icure/be/format/logic/impl/HealthOneLogicImplTest.kt
@@ -727,7 +727,7 @@ class HealthOneLogicImplTest {
 
         // Fourth parameter
         val full = true
-
+blabla
         // Executions
         val res1 = HealthOneLogicImpl.extractResultInfos(bufferedreader1, language, docID, full)
         val res2 = HealthOneLogicImpl.extractResultInfos(bufferedreader2, language, docID, full)
